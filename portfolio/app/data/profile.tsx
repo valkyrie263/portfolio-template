1f9a57d0e7f577f4118e04a7d49744bc96ef99c3
valkyrie263 <yankawadev@gmail.com>
2025-01-08 12:37:56 +0000

    [modify] プロフィールデータの分離

---
+++ portfolio/app/data/profile.tsx
@@ -0,0 +1,32 @@
+import {} from "react-skillicons";
+import { ProfileData } from "~/types";
+
+export const profile: ProfileData = {
+  name: "",
+  title: {
+    ja: "",
+    en: "",
+  },
+  bio: {
+    ja: "",
+    en: "",
+  },
+  avatar: "",
+  contacts: {
+    email: "",
+    github: "",
+    x: "",
+  },
+
+  // --- スキル ---
+  skills: [],
+
+  // --- 興味 ---
+  interests: {
+    ja: [],
+    en: [],
+  },
+
+  // --- 資格 ---
+  certifications: [],
+};
